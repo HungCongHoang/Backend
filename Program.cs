0941f984a5269675e7ad4fc9870ca665b1eb4536
HungCongHoang <hoangconghung189@gmail.com>
2023-07-02 09:55:54 +0700

    Learn C#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Vukhi sungluc = new Vukhi())
+        {
+            sungluc.name = "sung luc";
+            sungluc.tancong();
+
+        };
+        Vukhi sungmay = new Vukhi("Sung may", 5);
+        sungmay.tancong();
+    }
+}
---
+++ Vukhi.cs
@@ -0,0 +1,29 @@
+public class Vukhi : IDisposable
+{
+    public string name = "ten vu khi";
+    int dosatthuong = 1;
+    public Vukhi()
+    {
+        Console.WriteLine("da khoi tao");
+    }
+    public Vukhi(string name, int _dosatthuong)
+    {
+        this.name = name;
+        dosatthuong = _dosatthuong;
+    }
+
+    public void tancong()
+    {
+        Console.Write(name + ": ");
+        for (int i = 1; i <= dosatthuong; i++)
+        {
+            Console.Write(" * ");
+        }
+        Console.WriteLine();
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine("da huy");
+    }
+}
